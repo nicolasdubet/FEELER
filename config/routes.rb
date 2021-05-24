468e34cbba7926558df5bd0a58fd3da064b996b2
christophe mendy <christophe.mendy@kedgebs.com>
2021-05-24 17:51:09 +0200

    routesinfluencers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  resources :influencers, only: [:index, :show, ] do
+    resources :matches, only: :new
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
