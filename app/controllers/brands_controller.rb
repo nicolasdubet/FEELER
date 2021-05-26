5cdf870988c80b618ab74f3493ad579a55f77541
louis de Cheffontaines <louis2cheff@hotmail.fr>
2021-05-26 16:51:09 +0200

    new and create for brand

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,2 +1,32 @@
 class BrandsController < ApplicationController
+
+  def new
+    @brand = Brand.new
+  end
+
+  def create
+    @brand = Brand.new(brand_params)
+    @brand.user = current_user
+    if @brand.save
+        # code to set up congratulations message
+        redirect_to influencers_path # go to show page for @influencer
+      else
+        # code to set up error message
+        render :new
+    end
+  end
+
+
+
+  private
+
+  def brand_params
+    params.require(:brand).permit(:brand_name, :firsname, :lastname, :pseudo, :size, :category)
+  end
+
 end
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-   resources :influencers, only: [:index, :show] do
-     resources :matches, only: [:new, :create]
-   end
+  resources :influencers, only: [:index, :show] do
+    resources :matches, only: [:new, :create]
+  end
+
+  resources :brands
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # resources :brands, only: [:index, :show] do
   #   resources :campaigns, only: [:new, :create]
