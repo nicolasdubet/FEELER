b15a6a8163da33a9984895b9ae8b3b79eac49959
louis de Cheffontaines <louis2cheff@hotmail.fr>
2021-05-31 12:26:06 +0200

    commence à dev la card campaign

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -17,6 +17,10 @@ class CampaignsController < ApplicationController
        redirect_to influencers_path(@influencers)
     end
 
+  def show
+        @campaign = Campaign.find(params[:brand_id])
+        @campaign = Campaign.new
+    end
 
  private
 
